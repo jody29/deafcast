8ee18d3b1b0ac386540d7b546bf054e470268abb
Jody Lorist <66092262+jody29@users.noreply.github.com>
2022-04-22 09:31:55 +0200

    test

--- routers/homeRoute.js
+++ routers/homeRoute.js
@@ -5,7 +5,23 @@ const bodyParser = require('body-parser')
 
 router
 .get('/', (req, res) => {
-    res.render('pages/index')
+    const date = new Date().getHours()
+
+    const greeting = () => {
+        if (date >= 6 && date < 12) {
+            return 'Goedemorgen'
+        } else if (date >= 12 && date < 18) {
+            return 'Goedemiddag'
+        } else if (date >= 18 && date < 0) {
+            return 'Goedenavond'
+        } else if (date >= 0 && date < 6) {
+            return 'Goedenacht'
+        }
+    }
+    
+    res.render('pages/index', {
+        greeting: greeting()
+    })
 })
 
 module.exports = router
